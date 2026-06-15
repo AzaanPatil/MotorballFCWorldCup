823b9d029d155473f16a1b29de2f5cd4984ef45e
azaan <azaanpatil2@gmail.com>
2026-06-15 15:19:33 -0400

    Starting out with Menu system

---
+++ Assets/Scripts/MatchSettings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public static class MatchSettings
+{
+    public static GameManager.GameMode selectedMode;
+
+    public static bool playerIsHomeTeam;
+
+    public static VehicleController.VehicleType[] homeVehicles;
+    public static VehicleController.VehicleType[] awayVehicles;
+}
